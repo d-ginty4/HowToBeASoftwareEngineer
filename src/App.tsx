4b1c4b8e3faa1cef58500385a8900417b7e8d03e
d-ginty4 <d.ginty4@nuigalway.ie>
2023-03-30 21:10:49 +0100

    Added bulma and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { Navbar } from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Error } from "./pages/Error";
+
 function App() {
   return (
-    <h1>Main edited</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/HowToBeASoftwareEngineer" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Outlet, Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <><h1>Navbar</h1><Outlet /></>
+  )
+}
